988c68fe297eca658fda70d0a5f9d3c29ae59821
githubjinia <programmerjinia@gmail.com>
2022-02-25 19:23:20 +0600

    jsvascript

---
+++ data-types.js
@@ -0,0 +1,4 @@
+// let a = 12;
+// let a = 'I love my mom';
+let a = true;
+console.log(typeof a);
---
+++ true-false.js
@@ -0,0 +1,8 @@
+let x = {};
+console.log('type of x', x)
+if (x) {
+    console.log('variable is truthy');
+}
+else {
+    console.log('variable is falsy');
+}
